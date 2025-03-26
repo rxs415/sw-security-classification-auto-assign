2b93bb7fb829c1c77c0a8867e32f9d5a3cf1ba49
rxs415 <rocco.sorma@sherwin.com>
2025-03-26 12:43:50 -0400

    Update create_header_tokens.py

--- processing/create_header_tokens.py
+++ processing/create_header_tokens.py
@@ -1,1 +1,21 @@
+import requests
+import json
+import pandas as pd
+import urllib3
+urllib3.disable_warnings()
 
+
+
+def create_headers(host, credentials, token_headers, request_headers):
+    print(credentials)
+    # Sign in
+    url = host+'/auth/signin'
+    token = requests.post(url, json=credentials,headers=token_headers, verify=False)
+    print(token.request.body)
+    token_json = token.json()
+    request_headers['Authorization'] = 'Bearer ' + token_json['token']
+    headers = request_headers
+    # print(headers)
+    # print("20240103 DG/DQ - Connect to Collibra DQ as a data source test passed")
+    # print(headers)
+    return headers
